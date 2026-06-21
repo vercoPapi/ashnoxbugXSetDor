5a89d218d958422ca6cd118f653a76146a366b49
vercoPapi <muhbabangjantan@gmail.com>
2026-06-21 18:43:30 +0800

    Update auto.lua
    Added automatic processing function to scan for 'Wild' and perform actions.

--- auto.lua
+++ auto.lua
@@ -1,28 +1,31 @@
--- Konfigurasi Awal
-local baseBet = 100 -- Taruhan awal
-local currentBet = baseBet
-local winMultiplier = 2 -- Pengali setiap kali menang
+-- Konfigurasi
+local jumlah_loop = 500
+local nama_target = "Wild" -- Nama/Karakteristik yang dicari
 
--- Fungsi untuk menang
-function onWin(amount)
-    print("Selamat! Kamu menang: " .. amount)
-    
-    -- Logika Auto Double
-    currentBet = currentBet * winMultiplier
-    
-    print("Taruhan berikutnya dinaikkan menjadi: " .. currentBet)
-    return currentBet
+function proses_otomatis()
+    for i = 1, jumlah_loop do
+        -- 1. Pindai memori atau objek untuk menemukan "Wild"
+        -- Contoh: gg.searchNumber(nama_target, gg.TYPE_AUTO)
+        
+        local hasil_pencarian = -- (Fungsi untuk deteksi keberadaan Wild)
+        
+        if hasil_pencarian ~= nil then
+            -- 2. Jika "Wild" ditemukan, lakukan 500x perkalian/aksi
+            print("Wild ditemukan! Memulai aksi 500x...")
+            
+            for j = 1, 500 do
+                -- Masukkan aksi perkalian/modifikasi di sini
+                -- Contoh: gg.setValue(hasil_pencarian.address, nilai_baru)
+            end
+            
+            print("Aksi 500x selesai.")
+        else
+            print("Wild tidak ditemukan pada iterasi ke-" .. i)
+        end
+        
+        -- Delay untuk menjaga stabilitas sistem
+        -- gg.sleep(100) 
+    end
 end
 
--- Fungsi untuk kalah (Reset taruhan ke awal)
-function onLose()
-    print("Sayang sekali, kalah. Reset taruhan ke awal.")
-    currentBet = baseBet
-    return currentBet
-end
-
--- Simulasi penggunaan
-print("Mulai permainan dengan taruhan: " .. currentBet)
-currentBet = onWin(500) -- Simulasi menang
-currentBet = onWin(1000) -- Simulasi menang lagi
-currentBet = onLose()    -- Simulasi kalah
+proses_otomatis()
